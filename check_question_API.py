abac98cc62015b9efaff948df2984851ed242636
jiwonmik <jiwonmik@gmail.com>
2022-11-26 15:00:19 +0900

    add API for checking similarity

---
+++ check_question_API.py
@@ -0,0 +1,45 @@
+import spacy
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+import json
+
+
+app = FastAPI()
+
+nlp = spacy.load("en_core_web_sm")
+
+class Input(BaseModel):
+    question_text: str
+    keyword: str
+
+@app.post("/sentence_similarity")
+def check_sentence(input: Input):
+    question = nlp(input.question_text)
+    #question_token = [token for token in question]
+    
+    keyword = nlp(input.keyword)
+
+    similarity={
+        "similarity": question.similarity(keyword)
+    }
+
+    return similarity
+
+@app.post("/word_similarity")
+def check_word(input: Input):
+    question = nlp(input.question_text)
+    
+    keyword = nlp(input.keyword)
+
+    similarity={}
+    for token in question:
+        similarity[token.text] = token.similarity(keyword[0])
+    most_similar={}
+    for token in question:
+        s = token.similarity(keyword[0])
+        if s > 0:
+            most_similar[token.text] = s
+    return most_similar
+
