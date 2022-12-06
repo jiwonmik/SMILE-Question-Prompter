448c4a1b496d4532bc4652f0044533e77eefccd6
jiwonmik <jiwonmik@gmail.com>
2022-12-06 22:57:05 +0900

    add component for checking valid question

--- check_question.py
+++ check_question.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
+from operator import itemgetter
 import json
 
 app = FastAPI()
@@ -16,7 +17,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_lg")
 
 class Input(BaseModel):
     question: str
@@ -25,7 +26,6 @@ class Input(BaseModel):
 @app.post("/sentence_similarity")
 def check_sentence(input: Input):
     question = nlp(input.question)
-    #question_token = [token for token in question]
     
     keyword = nlp(input.keyword)
 
@@ -40,13 +40,50 @@ def check_word(input: Input):
     question = nlp(input.question)
     keyword = nlp(input.keyword)
 
-    similarity={"similarity":[],"most_similars":[]}
-    for token in question:
-        s = token.similarity(keyword[0])
+    question_token = [token.lemma_ for token in question]
+    vectors = [(word, nlp.vocab[word]) for word in question_token]
 
-        similarity["similarity"].append({"word": token.text, "similarity": round(s, 3)})
+    similarity={"similarity":[],"similars":[]}
 
-        if s > 0.2:
-            similarity["most_similars"].append({"word": token.text, "similarity": round(s,3)})
-    
+    for word, vector in vectors:
+        sim = vector.similarity(keyword[0])
+        similarity["similarity"].append({"word": word, "similarity": round(sim, 3)})
+
+        if sim > 0.1:
+            similarity["similars"].append({"word": word, "similarity": round(sim,3)})
+
+    most_similar=sorted(similarity["similars"], key=itemgetter('similarity'), reverse=True)[0]
+    similarity["most_similar"]={
+        "word": most_similar["word"],
+        "similarity": most_similar["similarity"]
+    }
     return similarity
+
+@app.post("/check_question")
+def check_word(input: Input):
+    question = nlp(input.question)
+    keywords = [nlp(word) for word in input.keyword.split(',')]
+
+    # lemmatization
+    question_token = [token.lemma_ for token in question]
+    vectors = [(word, nlp.vocab[word]) for word in question_token]
+
+    question_info={
+        "question": question.text,
+        "is_valid": False,
+        "included": []
+    }
+
+    for word, vector in vectors:
+        for keyword in keywords:
+            sim=vector.similarity(keyword)
+            included = { 
+                "question_token": word, 
+                "keyword": keyword.text,
+                "similarity": round(sim,3)}
+            if sim > 0.5:
+                question_info["is_valid"]=True
+                question_info["included"].append(included)
+                break
+
+    return question_info
