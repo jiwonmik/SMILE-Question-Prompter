af9e69188f40423c2d8af8de4749e3155f444b72
jiwonmik <jiwonmik@gmail.com>
2022-12-12 18:59:05 +0900

    function for checking question

--- js/demo.js
+++ js/demo.js
@@ -9,7 +9,6 @@ const sentInput1 = document.querySelector('#sents-form #sent1')
 const sentInput2 = document.querySelector('#sents-form #word')
 const sentSimil = document.querySelector('#sent-similarity')
 
-
 const HIDDEN_CLASSNAME = "hidden";
 
 function onWordSubmit(event){
@@ -55,5 +54,36 @@ function onSentSubmit(event){
     })  
 }
 
+const questionFrom = document.querySelector('#question-form')
+const question = document.querySelector('#question-form #question')
+const keywords = document.querySelector('#question-form #keywords')
+const isValid = document.querySelector('#is-valid')
+
+function onQuestionSubmit(event){
+    event.preventDefault();
+    const question_ = question.value;
+    const keyword_ = keywords.value;
+    const values = {
+        "question": question_,
+        "keyword": keyword_
+    }
+    const url = `http://127.0.0.1:8000/check_question`
+
+    axios.post(url, values)
+    .then((res) => {
+        console.log(res)
+        console.log(res.data)
+        data=res.data
+        included=res.data.included[0]
+        if (data.is_valid)
+            isValid.innerText = `It is a valid question.`;
+        else
+            isValid.innerText = `It is not a valid question.`;
+
+        isValid.classList.remove(HIDDEN_CLASSNAME);
+    })  
+}
+
 wordForm.addEventListener("submit", onWordSubmit)
 sentForm.addEventListener("submit", onSentSubmit)
+questionFrom.addEventListener("submit", onQuestionSubmit)
