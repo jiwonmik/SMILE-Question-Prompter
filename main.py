36de242ab4553e7c96d0cd16fdfeccd156967116
jiwonmik <jiwonmik@gmail.com>
2023-01-05 11:02:45 +0000

    add write_req_res function for recording request and reponse to csv file

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import spacy
 
 from fastapi import FastAPI, Request, Response
 from starlette.background import BackgroundTask
+from starlette.concurrency import iterate_in_threadpool
 from pydantic import BaseModel
 from starlette.types import Message
 from fastapi.middleware.cors import CORSMiddleware
 
 from operator import itemgetter
-import json
+import json, csv
 import logging
 
 logger = logging.getLogger("main")
@@ -19,7 +20,8 @@ stream_hander = logging.StreamHandler()
 stream_hander.setFormatter(formatter)
 logger.addHandler(stream_hander)
 
-file_handler = logging.FileHandler('info.log', mode='w')
+file_handler = logging.FileHandler('info.log')
+file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
 origins = ["*"]
@@ -35,15 +37,16 @@ app.add_middleware(
 
 nlp = spacy.load("en_core_web_lg")
 
-# def write_req_res(id, c1, c2, similarity):
-#     data = {'id': id, 'c1': c1, 'c2': c2, 'similarity': similarity}
-#     df = pd.DataFrame(data)
-#     with open('/home/jiwon/log.csv', 'a') as f:
-#         df.to_csv(f, header=False)
+def write_req_res(data):
+    with open('/home/jiwon/log.csv', 'a') as f:
+        keys = data[0].keys()
+        dict_writer = csv.DictWriter(f, fieldnames=keys)
+        dict_writer.writerows(data)
 
 def log_info(req_body, res_body):
     logger.info(req_body)
     logger.info(res_body)
+    
 
 async def set_body(request: Request, body: bytes):
     async def receive() -> Message:
@@ -53,29 +56,48 @@ async def set_body(request: Request, body: bytes):
 @app.middleware("http")
 async def some_middleware(request: Request, call_next):
     req_body = await request.body()
+    req_json = await request.json()
     await set_body(request, req_body)
     response = await call_next(request)
 
+    # write to info.log file
     res_body = b''
     async for chunk in response.body_iterator:
         res_body += chunk
 
-    task = BackgroundTask(log_info, req_body, res_body)
+    # task = BackgroundTask(log_info, req_body, res_body)
+    
+    # write to log.csv file
+    data = []
+    res_str = res_body.decode('utf8')
+    res_json = json.loads(res_str)
+    print(req_json["keywords"])
+    for i in res_json["included"]:
+        record = {'id_': req_json["id_"], 
+                'question': req_json["question"], 
+                'keywords': req_json["keywords"], 
+                'question_token': i["question_token"],
+                'keyword': i["keyword"],
+                'similarity': i["similarity"]}
+        data.append(record)
+    task = BackgroundTask(write_req_res, data)
+
     return Response(content=res_body, status_code=response.status_code, 
         headers=dict(response.headers), media_type=response.media_type, background=task)
 
 class Input(BaseModel):
+    id_: str
     question: str
-    keyword: str
+    keywords: str
 
 @app.post("/sentence_similarity")
 def check_sentence(input: Input):
     question = nlp(input.question)
     
-    keyword = nlp(input.keyword)
+    keywords = nlp(input.keywords)
 
     similarity={
-        "similarity": round(question.similarity(keyword),3)
+        "similarity": round(question.similarity(keywords),3)
     }
 
     return similarity
@@ -83,7 +105,7 @@ def check_sentence(input: Input):
 @app.post("/word_similarity")
 def check_word(input: Input):
     question = nlp(input.question)
-    keyword = nlp(input.keyword)
+    keywords = nlp(input.keywords)
 
     question_token = [token.lemma_ for token in question]
     vectors = [(word, nlp.vocab[word]) for word in question_token]
@@ -91,7 +113,7 @@ def check_word(input: Input):
     similarity={"similarity":[],"similars":[]}
 
     for word, vector in vectors:
-        sim = vector.similarity(keyword[0])
+        sim = vector.similarity(keywords[0])
         similarity["similarity"].append({"word": word, "similarity": round(sim, 3)})
 
         if sim > 0.1:
@@ -107,7 +129,7 @@ def check_word(input: Input):
 @app.post("/check_question")
 def check_word(input: Input):
     question = nlp(input.question)
-    keywords = [nlp(word) for word in input.keyword.split(',')]
+    keywords = [nlp(word) for word in input.keywords.split(',')]
 
     # lemmatization
     question_token = [token.lemma_ for token in question]
