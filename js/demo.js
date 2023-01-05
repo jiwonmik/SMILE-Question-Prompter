1c90fc07fc0d5539ecdc58acee238f576102cf9b
jiwonmik <jiwonmik@gmail.com>
2023-01-05 14:04:30 +0000

    delete components for word similarity

--- js/demo.js
+++ js/demo.js
@@ -1,39 +1,9 @@
-const wordForm = document.querySelector('#words-form')
-const wordInput1 = document.querySelector('#words-form #word1')
-const wordInput2 = document.querySelector('#words-form #word2')
-const simWord = document.querySelector('#most-sim-word')
-const simValue = document.querySelector('#most-sim-value')
-
 const sentForm = document.querySelector('#sents-form')
 const sentInput1 = document.querySelector('#sents-form #sent1')
 const sentInput2 = document.querySelector('#sents-form #word')
 const sentSimil = document.querySelector('#sent-similarity')
 
 const HIDDEN_CLASSNAME = "hidden";
-
-function onWordSubmit(event){
-    event.preventDefault();
-    const word1 = wordInput1.value;
-    const word2 = wordInput2.value;
-    const values = {
-        "id_": "ww",
-        "question": word1,
-        "keywords": word2
-    }
-    const url = `http://127.0.0.1:8000/word_similarity`
-
-    axios.post(url, values)
-        .then((res) => {
-            console.log(res)
-            console.log(res.data)
-            most_sim_word = res.data.most_similar.word
-            most_sim_value = res.data.most_similar.similarity
-            simWord.innerText = `most similar word: ${most_sim_word}`;
-            simWord.classList.remove(HIDDEN_CLASSNAME);
-            simValue.innerText = `similarity: ${most_sim_value}`
-            simValue.classList.remove(HIDDEN_CLASSNAME);
-        })    
-}
 
 function onSentSubmit(event){
     event.preventDefault();
