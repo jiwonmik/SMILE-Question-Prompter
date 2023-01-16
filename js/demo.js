87fe6dae91aab79e51a2eace5bba9e179229d1cb
jiwonmik <jiwonmik@gmail.com>
2023-01-16 13:23:20 +0000

    add spacy korean model

--- js/demo.js
+++ js/demo.js
@@ -5,7 +5,7 @@ const sentSimil = document.querySelector('#sent-similarity')
 
 const HIDDEN_CLASSNAME = "hidden";
 
-function onSentSubmit(event){
+function onSentSubmit(event) {
     event.preventDefault();
     const sent = sentInput1.value;
     const s_word = sentInput2.value;
@@ -17,13 +17,13 @@ function onSentSubmit(event){
     const url = `http://45.32.89.216/similarity/sentence`
 
     axios.post(url, values)
-    .then((res) => {
-        console.log(res)
-        console.log(res.data)
-        sim_value = res.data.similarity
-        sentSimil.innerText = `similarity: ${sim_value}`;
-        sentSimil.classList.remove(HIDDEN_CLASSNAME);
-    })  
+        .then((res) => {
+            console.log(res)
+            console.log(res.data)
+            sim_value = res.data.similarity
+            sentSimil.innerText = `similarity: ${sim_value}`;
+            sentSimil.classList.remove(HIDDEN_CLASSNAME);
+        })
 }
 
 const questionFrom = document.querySelector('#question-form')
@@ -31,7 +31,7 @@ const question = document.querySelector('#question-form #question')
 const keywords = document.querySelector('#question-form #keywords')
 const isValid = document.querySelector('#is-valid')
 
-function onQuestionSubmit(event){
+function onQuestionSubmit(event) {
     event.preventDefault();
     isValid.classList.add(HIDDEN_CLASSNAME);
     const question_ = question.value;
@@ -44,19 +44,52 @@ function onQuestionSubmit(event){
     const url = `http://45.32.89.216/similarity/question`
 
     axios.post(url, values)
-    .then((res) => {
-        console.log(res)
-        console.log(res.data)
-        data=res.data
-        included=res.data.included[0]
-        if (data.is_valid)
-            isValid.innerText = `It is a valid question.`;
-        else
-            isValid.innerText = `It is not a valid question.`;
+        .then((res) => {
+            console.log(res)
+            console.log(res.data)
+            data = res.data
+            included = res.data.included[0]
+            if (data.is_valid)
+                isValid.innerText = `It is a valid question.`;
+            else
+                isValid.innerText = `It is not a valid question.`;
 
-        isValid.classList.remove(HIDDEN_CLASSNAME);
-    })  
+            isValid.classList.remove(HIDDEN_CLASSNAME);
+        })
+}
+
+const kquestionFrom = document.querySelector('#k-question-form')
+const kquestion = document.querySelector('#k-question-form #k-question')
+const kkeywords = document.querySelector('#k-question-form #k-keywords')
+const kisValid = document.querySelector('#k-is-valid')
+
+function onkQuestionSubmit(event) {
+    event.preventDefault();
+    kisValid.classList.add(HIDDEN_CLASSNAME);
+    const question_ = kquestion.value;
+    const keyword_ = kkeywords.value;
+    const values = {
+        "id_": "qk",
+        "question": question_,
+        "keywords": keyword_
+    }
+    const url = `http://45.32.89.216/similarity/question/korean`
+
+    axios.post(url, values)
+        .then((res) => {
+            console.log(res)
+            console.log(res.data)
+            data = res.data
+            included = res.data.included[0]
+            if (data.is_valid)
+                kisValid.innerText = `It is a valid question.`;
+            else
+                kisValid.innerText = `It is not a valid question.`;
+
+            kisValid.classList.remove(HIDDEN_CLASSNAME);
+        })
 }
 
 sentForm.addEventListener("submit", onSentSubmit)
 questionFrom.addEventListener("submit", onQuestionSubmit)
+kquestionFrom.addEventListener("submit", onkQuestionSubmit)
--- main.py
+++ main.py
@@ -15,13 +15,14 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["45.32.89.216", "192.168.219.108"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 nlp = spacy.load("en_core_web_lg")
+konlp = spacy.load("ko_core_news_lg")
 
 def write_req_res(data, filepath):
     with open(filepath, 'a') as f:
@@ -54,6 +55,60 @@ def check_sentence(input: Input):
 @app.get("/")
 def test():
     return {"hello": "world"}
+
+
+@app.post("/similarity/question/korean")
+def check_word(input: Input):
+    id_ = input.id_
+    question = konlp(input.question)
+    keywords = [konlp(word) for word in input.keywords.split(',')]
+
+    # lemmatization
+    lemmas = [token.lemma_ for token in question]
+    question_token = [lemma.split("+")[0] for lemma in lemmas]
+    vectors = [(word, konlp.vocab[word]) for word in question_token]
+
+    question_info={
+        "question": question.text,
+        "is_valid": False,
+        "included": [],
+        "not_included":[]
+    }
+    data=[]
+    record = {'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        'id_': id_,
+        'question': input.question,
+        'keywords': input.keywords,
+        'question_token': "",
+        'keyword': "",
+        'similarity': 0}
+    for word, vector in vectors:
+        for keyword in keywords:
+            sim=vector.similarity(keyword)
+            included = { 
+                "question_token": word, 
+                "keyword": keyword.text,
+                "similarity": round(sim,3)}
+            if sim > 0.5:
+                question_info["is_valid"]=True
+                question_info["included"].append(included)
+
+                new_record=record
+                new_record["question_token"] = included["question_token"]
+                new_record["keyword"] = included["keyword"]
+                new_record["similarity"] = included["similarity"]
+
+                data.append(record)
+            else:
+                question_info["not_included"].append(included)
+    # write to qk_log.csv file
+    if question_info["is_valid"] == False:
+        data.append(record)
+
+    # write_req_res(data, '/home/jiwon/myapi/logs/qk_log.csv')
+    
+    return question_info
+
 
 @app.post("/similarity/question")
 def check_word(input: Input):
