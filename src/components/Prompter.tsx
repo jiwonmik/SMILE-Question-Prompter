11156c00dd8ffa1bfceec72232294c9207a073cb
jiwonmik <jiwonmik@gmail.com>
2023-03-28 17:21:40 +0900

    feat: change keyword input and display layout 1. keyword can be added word by word 2. User can delete specific keyword by delete button in each keyword component

--- src/components/Prompter.tsx
+++ src/components/Prompter.tsx
@@ -1,15 +1,50 @@
-import { Textarea, Button, Text, Box, Alert, AlertIcon } from '@chakra-ui/react';
+import {
+  Textarea,
+  Button,
+  Text,
+  Box,
+  Alert,
+  AlertIcon,
+  Tag,
+  TagLabel,
+  HStack,
+  Input,
+  Grid,
+  TagCloseButton,
+} from '@chakra-ui/react';
 import { useState } from 'react';
-import { Input, SimilarityRes } from 'types/api.type';
+import { IInput, IReqBody, SimilarityRes } from 'types/api.type';
 import { fetchData, fetchKorData } from 'api/api';
 import { PrompterProps } from 'types/type';
 
 function Prompter({ option }: PrompterProps) {
   const InitialInput = {
     question: '',
-    keywords: '',
+    keyword: '',
   };
-  const [input, setInput] = useState<Input>(InitialInput);
+  const [input, setInput] = useState<IInput>(InitialInput);
+  const [keywords, setKeywords] = useState<string[]>([]);
+
+  const onAddHandle = () => {
+    setKeywords((prev) => {
+      return [...prev, ...input.keyword.replace(' ', '').split(',')];
+    });
+    setInput((prev) => {
+      return { ...prev, keyword: '' };
+    });
+  };
+  const onResetHandle = () => {
+    setKeywords(() => {
+      return [];
+    });
+  };
+
+  const onDeleteHandle = (index: number) => {
+    setKeywords((prev) => {
+      return [...prev.slice(0, index), ...prev.slice(index + 1)];
+    });
+  };
+
   const [similairty, setSimilarity] = useState<SimilarityRes>();
   const questionPlaceholder =
     option === 'eng'
@@ -18,9 +53,13 @@ function Prompter({ option }: PrompterProps) {
   const keywordsPlaceholder = option === 'eng' ? 'ex. Individuals, computers' : 'ex. 파형, 필요';
 
   const OnCheckHandle = () => {
+    const body = {
+      question: input?.question,
+      keywords: keywords.join(', '),
+    } as IReqBody;
     switch (option) {
       case 'eng': {
-        fetchData(input).then((res) => {
+        fetchData(body).then((res) => {
           setSimilarity(() => {
             return {
               question: res?.data.question,
@@ -32,8 +71,7 @@ function Prompter({ option }: PrompterProps) {
         break;
       }
       case 'kor': {
-        fetchKorData(input).then((res) => {
-          console.log(res.data);
+        fetchKorData(body).then((res) => {
           setSimilarity(() => {
             return {
               question: res?.data.question,
@@ -48,6 +86,28 @@ function Prompter({ option }: PrompterProps) {
 
   return (
     <>
+      <HStack mb="5">
+        <Input
+          value={input?.keyword}
+          onChange={(e) =>
+            setInput((prev) => {
+              return { ...prev, keyword: e.target.value };
+            })
+          }
+          placeholder={keywordsPlaceholder}
+          resize={'none'}
+        />
+        <Button onClick={onAddHandle}>Add</Button>
+        <Button onClick={onResetHandle}>Reset</Button>
+      </HStack>
+      <Grid templateColumns="repeat(4, 1fr)" gap={5} mb="5">
+        {keywords.map((keyword, index) => (
+          <Tag key={index} size="lg" colorScheme={'red'} borderRadius="full" width={'fit-content'}>
+            <TagLabel>{keyword}</TagLabel>
+            <TagCloseButton onClick={() => onDeleteHandle(index)} />
+          </Tag>
+        ))}
+      </Grid>
       <Textarea
         value={input?.question}
         onChange={(e) =>
@@ -57,18 +117,6 @@ function Prompter({ option }: PrompterProps) {
         }
         my={5}
         placeholder={questionPlaceholder}
-        width="full"
-        height="170px"
-        resize={'none'}
-      />
-      <Textarea
-        value={input?.keywords}
-        onChange={(e) =>
-          setInput((prev) => {
-            return { ...prev, keywords: e.target.value };
-          })
-        }
-        placeholder={keywordsPlaceholder}
         width="full"
         height="100px"
         resize={'none'}
@@ -80,7 +128,6 @@ function Prompter({ option }: PrompterProps) {
         <>
           <Box borderRadius={'md'} bg="red.100" padding={'5'} mb={'5'}>
             <Text>Your question: {similairty?.question}</Text>
-            <Text>Your keywords: {input.keywords}</Text>
           </Box>
           {similairty?.valid ? (
             <Alert status="success">
