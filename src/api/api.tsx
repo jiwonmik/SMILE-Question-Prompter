c4c9f370a204d1723864c8c90b04eb7904bd833e
jiwonmik <jiwonmik@gmail.com>
2023-03-28 14:17:01 +0900

    fix: change api url to https

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Input } from 'types/api.type';
 
-const BASE_URL = 'http://45.32.89.216/similarity/question';
+const BASE_URL = 'https://www.smile-similarity.org/similarity/question';
 
 export const fetchData = async (input: Input) => {
   const response = await axios.post(`${BASE_URL}`, input);
